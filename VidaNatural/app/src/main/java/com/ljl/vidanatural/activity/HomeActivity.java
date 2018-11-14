30aee8645ae14ba6bf562e70efc3990ad594b7fe
LuizFStival <lfstival@gmail.com>
2018-11-14 13:13:05 -0200

    HomeActivity

---
+++ VidaNatural/app/src/main/java/com/ljl/vidanatural/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.ljl.vidanatural.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ljl.vidanatural.R;
+
+public class HomeActivity    extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
